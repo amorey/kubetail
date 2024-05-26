dbfebeeda3a59425753b0bf17ea2804a486c7f75
Andres Morey <andresmarcel@gmail.com>
2024-05-26 20:00:16 +0300

    wip

--- backend/agent/main.go
+++ backend/agent/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/nats-io/nats.go"
+)
 
 func main() {
 	fmt.Println("agent")
+
+	// initialize context and listen for termination signals
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// Connect to a nats server
+	nc, err := nats.Connect("nats://nats:4222")
+	if err != nil {
+		panic(err)
+	}
+	defer nc.Close()
+
+	// async subscriber
+	nc.Subscribe("foo", func(m *nats.Msg) {
+		fmt.Println(m)
+	})
+
+	// wait for context
+	<-ctx.Done()
+	stop() // stop receiving signals as soon as possible
+
+	fmt.Println("exiting")
 }
